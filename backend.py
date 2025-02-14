c111481eef2146cec7318c26bbf4ee7f021efe48
Dharshan <146535811+Dharshan0074@users.noreply.github.com>
2025-02-14 19:29:55 +0530

    Add files via upload
    1️⃣ Integrated Google AI Studio to use the Gemini API for generating intelligent responses.
    2️⃣ Designed the chatbot UI using Streamlit, ensuring a sleek and user-friendly interface.
    3️⃣ Implemented real-time response streaming to enhance engagement and make conversations feel natural.

---
+++ backend.py
@@ -0,0 +1,33 @@
+import os
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyAzlWn5cUmQ1_hqhszpIFB5ALC99DGj-T4")
+
+# Create the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-2.0-flash",
+  generation_config=generation_config,
+)
+def GenerateResponse(input_text):
+    response = model.generate_content([
+    "input: who are you ?",
+    "output: I am Dharshan's chatbot Assistant",
+    "input: What all can you do?",
+    "output: We help people to build innovative projects",
+    f"input: {input_text}",
+    "output: ",
+    ])
+    return response.text
+
+#while True:
+    #string = str(input("Enter your prompt:"))
+    #print("Bot:", GenerateResponse(string))
+    
